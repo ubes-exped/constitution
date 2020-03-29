6baa72fa54840c438c5ad1bd50c923cb3e9de2f4
Charlie Harding <charlie_harding@icloud.com>
2020-03-29 14:28:42 +0100

    Print PRs

--- .github/action/index.js
+++ .github/action/index.js
@@ -4,11 +4,14 @@ const exec = require('@actions/exec');
 
 (async () => {
   try {
+    const token = core.getInput('github-token');
+
     await exec.exec('git symbolic-ref HEAD refs/heads/gh-pages', [], {
       silent: true
     });
 
     await exec.exec('git status', [], {
+      silent: true,
       listeners: {
         stdline(line) {
           console.log(line);
@@ -16,8 +19,15 @@ const exec = require('@actions/exec');
       }
     });
 
-    const time = new Date().toTimeString();
-    core.setOutput('time', time);
+    const octokit = new github.GitHub(token);
+
+    const pulls = await octokit.pulls.list({
+      owner: github.context.repo.owner,
+      repo: github.context.repo.repo
+    });
+
+    console.log(pulls);
+
     // Get the JSON webhook payload for the event that triggered the workflow
     const payload = JSON.stringify(github.context.payload, undefined, 2);
     console.log(`The event payload: ${payload}`);
