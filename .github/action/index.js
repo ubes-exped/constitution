7787f056822b50a37b92e31da558d3b53a51d32d
Charlie Harding <charlie_harding@icloud.com>
2020-03-29 13:14:23 +0100

    Test git in JS action

--- .github/action/index.js
+++ .github/action/index.js
@@ -1,27 +1,21 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
-const git = require('nodegit');
+const exec = require('@actions/exec');
 
 (async () => {
   try {
-    // `who-to-greet` input defined in action metadata file
-    const nameToGreet = core.getInput('who-to-greet');
-    const repo = await git.Repository.open();
-    await repo.checkoutBranch('master');
-    await git.Reference.symbolicCreate(repo, 'HEAD', 'refs/heads/gh-pages', 1);
-    const { exec } = require('child_process');
-    exec('git status', (err, stdout, stderr) => {
-      if (err) {
-        //some err occurred
-        throw new Error(err);
-      } else {
-        // the *entire* stdout and stderr (buffered)
-        console.log(`stdout: ${stdout}`);
-        console.log(`stderr: ${stderr}`);
+    await exec.exec('git symbolic-ref HEAD refs/heads/gh-pages', [], {
+      silent: true
+    });
+
+    await exec.exec('git status', [], {
+      listeners: {
+        stdline(line) {
+          console.log(line);
+        }
       }
     });
 
-    console.log(`Hello ${nameToGreet}!`);
     const time = new Date().toTimeString();
     core.setOutput('time', time);
     // Get the JSON webhook payload for the event that triggered the workflow
