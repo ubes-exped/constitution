25f4e76cd5ab198fd0cc9facc9d2de4475007c8e
Charlie Harding <charlie_harding@icloud.com>
2020-03-29 16:46:15 +0100

    Filter PRs to only those eventually on base master

--- .github/action/index.js
+++ .github/action/index.js
@@ -1,57 +1,112 @@
+// @ts-check
+
 const core = require('@actions/core');
 const github = require('@actions/github');
 const exec = require('@actions/exec');
 const fs = require('mz/fs');
 
+/** @typedef {import('@octokit/rest').Octokit.PullsListResponseItem } PullsListResponseItem */
+
 (async () => {
   try {
-    const token = core.getInput('repo-token');
-
-    await exec.exec('git symbolic-ref HEAD refs/heads/gh-pages', [], {
-      silent: true
-    });
-
-    await exec.exec('git status', [], {
-      silent: true,
-      listeners: {
-        stdline(line) {
-          console.log(line);
-        }
-      }
-    });
-
-    const octokit = new github.GitHub(token);
-
-    const { data: pulls } = await octokit.pulls.list({
-      owner: github.context.repo.owner,
-      repo: github.context.repo.repo
-    });
-
-    const root = github.context.repo.repo;
-    console.log('root', root);
-
-    await fs.mkdir('./gh-pages/_amendments', { recursive: true });
-    await fs.readdir('./gh-pages/_amendments');
-
-    for (const pull of pulls) {
-      console.log(JSON.stringify(pull, undefined, 2));
-      const base = pull.base.label;
-      const source = pull.base.label;
-      console.log(
-        'listfiles',
-        await octokit.pulls.listFiles({
-          owner: github.context.repo.owner,
-          repo: github.context.repo.repo,
-          pull_number: pull.number
-        })
-      );
-      const commit = pull.merge_commit_sha;
-    }
-
-    // Get the JSON webhook payload for the event that triggered the workflow
-    const payload = JSON.stringify(github.context.payload, undefined, 2);
-    console.log(`The event payload: ${payload}`);
+    await main();
   } catch (error) {
     core.setFailed(error.message);
   }
 })();
+
+async function main() {
+  const token = core.getInput('repo-token');
+
+  await exec.exec('git symbolic-ref HEAD refs/heads/gh-pages', [], {
+    silent: true
+  });
+
+  await exec.exec('git status', [], {
+    silent: true,
+    listeners: {
+      stdline(line) {
+        console.log(line);
+      }
+    }
+  });
+
+  const octokit = new github.GitHub(token);
+
+  const { data: allPulls } = await octokit.pulls.list({
+    owner: github.context.repo.owner,
+    repo: github.context.repo.repo
+  });
+
+  const root = github.context.repo.owner;
+  const rootBranch = `${root}:master`;
+
+  const pulls = filterPRs(allPulls, rootBranch);
+
+  await fs.mkdir('./gh-pages/_amendments', { recursive: true });
+  const files = await fs.readdir('./gh-pages/_amendments');
+
+  console.log(files);
+
+  for (const [baseRef, pull] of pulls) {
+    console.log(JSON.stringify(pull, undefined, 2));
+    const sourceRef = pull.base.label;
+    console.log(
+      'listfiles',
+      await octokit.pulls.listFiles({
+        owner: github.context.repo.owner,
+        repo: github.context.repo.repo,
+        pull_number: pull.number
+      })
+    );
+    const commit = pull.merge_commit_sha;
+  }
+
+  // Get the JSON webhook payload for the event that triggered the workflow
+  const payload = JSON.stringify(github.context.payload, undefined, 2);
+  console.log(`The event payload: ${payload}`);
+}
+
+function filterPRs(
+  /** @type {PullsListResponseItem[]} */ pulls,
+  /** @type {String} */ rootBranch
+) {
+  /** @type {String[]} */ const rootChildren = [];
+
+  /** @type {Map<String, PullsListResponseItem & { children: String[] }>} */ const pullsByRef = new Map();
+  for (const pull of pulls) {
+    const headRef = pull.head.label;
+    if (pullsByRef.has(headRef)) {
+      core.warning(
+        `Two pulls with same head ${headRef}, ignoring #${pull.number}.`
+      );
+      continue;
+    }
+    pullsByRef.set(headRef, { ...pull, children: [] });
+  }
+
+  for (const pull of pulls) {
+    const headRef = pull.head.label;
+    const baseRef = pull.base.label;
+    if (baseRef === rootBranch) rootChildren.push(headRef);
+    else {
+      const base = pullsByRef.get(baseRef);
+      if (base) base.children.push(headRef);
+    }
+  }
+  /** @type {Set<String>} */ const goodRefs = new Set();
+
+  /** @type {(ref: String) => void} */ const markChildren = ref => {
+    goodRefs.add(ref);
+    pullsByRef.get(ref).children.forEach(markChildren);
+  };
+  rootChildren.forEach(markChildren);
+
+  for (const [ref, pull] of pullsByRef) {
+    if (!goodRefs.has(ref)) {
+      console.warn(`Ignoring #${pull.number} with base ${pull.base.label}.`);
+      pullsByRef.delete(ref);
+    }
+  }
+  return pullsByRef;
+}
